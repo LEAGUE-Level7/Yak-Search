a5154b1f1bc06725d0ed1a28a244027b172cd0c7
League Student <league@iMac-22.attlocal.net>
2022-03-27 16:07:48 -0700

    initial

---
+++ src/main/java/org/jointheleague/api/yak/cityweather/CityWeather/CityWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.yak.cityweather.CityWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityWeatherApplication.class, args);
+	}
+
+}
