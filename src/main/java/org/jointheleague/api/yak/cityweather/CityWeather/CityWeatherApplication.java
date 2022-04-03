aaa74d22a57fb9c48bd2a1226dc3dcb331411762
League Student <league@iMac-14.attlocal.net>
2022-04-03 16:31:31 -0700

    Changed .settings to .yml
    Renamed the application.settings file to application.yml. Also added some dependencies in build.gradle to fix depreciation issues.

--- src/main/java/org/jointheleague/api/yak/cityweather/CityWeather/CityWeatherApplication.java
+++ src/main/java/org/jointheleague/api/yak/cityweather/CityWeather/CityWeatherApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CityWeatherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CityWeatherApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(CityWeatherApplication.class, args);}
 
 }
