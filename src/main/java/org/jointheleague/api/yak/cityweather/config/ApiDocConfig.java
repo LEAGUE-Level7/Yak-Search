b77198054e96510ebcdcf0e7f4a3049eb100cdc5
League Student <league@iMac-14.attlocal.net>
2022-04-04 12:08:08 -0700

    Not working yet

---
+++ src/main/java/org/jointheleague/api/yak/cityweather/config/ApiDocConfig.java
@@ -0,0 +1,40 @@
+package org.jointheleague.api.yak.cityweather.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class ApiDocConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Level 7 Weather Application",
+                "An application that uses an api to return weather information.",
+                "1.0.0",
+                null,
+                new Contact("Riley Zhu", "www.jointheleague.org", "rzhu.mathleague@gmail.com"),
+                null, null, Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+}
+
